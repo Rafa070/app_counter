93ceb727bdc644bd2e7b93850f39157096a51270
Afonso Henrique <hafonso021@gmail.com>
2022-07-25 09:24:21 -0300

    feat(__tests__): Update tests Input Component Login

--- __tests__/src/pages/Login/Index-test.js
+++ __tests__/src/pages/Login/Index-test.js
@@ -19,12 +19,12 @@ describe('Testes de Inicialização e Exibição', () => {
 describe ('Testes do componente Input', () => {
     it('Teste de input email tela Login', () => {
       const { getByTestId} = render(<App />);
-      fireEvent.press(getByTestId("btnCadastro"));
+      fireEvent.press(getByTestId("btnLogin"));
       fireEvent.press(getByTestId("inputEmail"));
     })
     it('Teste de input Senha tela Login', () => {
       const { getByTestId} = render(<App />);
-      fireEvent.press(getByTestId("btnCadastro"));
+      fireEvent.press(getByTestId("btnLogin"));
       fireEvent.press(getByTestId("inputSenha"));
     })
 })
